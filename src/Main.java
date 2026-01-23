f82fe2418e2e3c569ce3104b6c54de09c1a0db9c
wengtim <wengtim05@proton.me>
2026-01-23 15:35:04 +0800

    feat: rebasing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        new userSelect();
-    }
+  public static void main(String[] args) {
+    // new userSelect();
+    new lecturerDashboard();
+  }
 }
